2977b20344da360b799921bc0812aeb8772fad14
MastaCoda <68067233+Sul70@users.noreply.github.com>
2021-05-06 18:16:11 -0500

    My SUOS 10 Project

--- Windows 10 DOS Mode/MSDOS Mode.bat
+++ Windows 10 DOS Mode/MSDOS Mode.bat
@@ -32,7 +32,7 @@ setlocal & pushd .
 cd /d %~dp0
 if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 
-REM ~-~-~-~-~-~-~-~ Batch file starts hered
+REM ~-~-~-~-~-~-~-~ Batch file starts here
 echo.
 echo Starting the file... 
 ping localhost -n 2 > nul
