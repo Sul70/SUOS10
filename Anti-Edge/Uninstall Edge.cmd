e703a3a956565e622a8f51445c53f3e656af37cf
MastaCoda <68067233+Sul70@users.noreply.github.com>
2021-05-06 18:10:37 -0500

    Anti-Edge Files.

---
+++ Anti-Edge/Uninstall Edge.cmd
@@ -0,0 +1,10 @@
+@echo off
+cd /d "%~dp0"
+echo Uninstalling Microsoft Edge...
+CLS
+install_wim_tweak.exe /o /l
+install_wim_tweak.exe /o /c Microsoft-Windows-Internet-Browser-Package /r
+install_wim_tweak.exe /h /o /l
+echo Microsoft Edge should be uninstalled. Please reboot Windows.
+pause
+echo You uninstalled Edge. -The SUOS Team
